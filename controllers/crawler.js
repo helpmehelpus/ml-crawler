2717d159af6fbd6ac220e75935a33c89f06ee12f
adriano <adrianortorres@gmail.com>
2020-08-05 02:46:08 -0300

    captured loja field

--- controllers/crawler.js
+++ controllers/crawler.js
@@ -13,22 +13,77 @@ class Crawler {
       console.log(error);
     }
     const $ = cheerio.load(res.data);
-    $('.results-item').each((i, item) => {
+    const partialResult = await this.getPartialObj($);
+    const fullResult = await this.getFullResult(partialResult);
+    // let productPage;
+    // try {
+    //   productPage = await axios.get(link)
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    // const $product = cheerio.load(productPage.data);
+    // const storeLink = $product('#seller-view-more-link').attr('href');
+    // if (storeLink) {
+    //   console.log(`close to name of store: ${storeLink}`);
+    // }
+    return fullResult;
+  }
+  async getPartialObj(object) {
+    const result = [];
+    let name;
+    let link;
+    let price;
+    console.log(object);
+      const $ = object;
+      $('.results-item').each((i, item) => {
       console.log(i);
-      console.log(`name:` +  $(item).find('.main-title').text());
-      const link = $('.item__info-title').attr('href')
+
+      name = $(item).find('.main-title').text()
+      console.log(`name: ${name}`);
+
+      link = $(item).find('.item__info-title').attr("href");
       console.log(`link: ${link}`);
-      const worker = new Worker(workerPath, { workerData: link });
-      console.log('Sending product URI to worker...');
-      worker.on('message', (message) => {
-        console.log('received link back from worker');
-      });
+
+      // const worker = new Worker(workerPath, { workerData: { link, workerIndex: i } });
+      // console.log('Sending product URI to worker...');
+
+      // worker.on('message', (message) => {
+      //   console.log('received link back from worker');
+      // });
+
       const fraction = $(item).find('.price__fraction').text();
       const decimals = $(item).find('.price__decimals').text();
-      const price = parseFloat(fraction + "." + decimals);
-      // console.log(`price: ${price}`);
+      price = parseFloat(fraction + "." + decimals);
+      console.log(`price: ${price}`);
+      result.push({ name, link, price });
     });
-    return { code: 200};
+    return result;
+  }
+
+  async getFullResult(objects) {
+    const result = [];
+    const promises = objects.map(async item => {
+      let res;
+      try {
+        res = await axios.get(item.link);
+      } catch (error) {
+        console.log(error);
+      }
+      const $ = cheerio.load(res.data);
+      const link = $('#seller-view-more-link').attr('href');
+      const newObject = {...item};
+      let loja = link.split("/");
+      let nomeLoja = loja[loja.length - 1];
+      console.log(link);
+      console.log(nomeLoja);
+      newObject.loja = decodeURIComponent(nomeLoja);
+      console.log(newObject);
+      result.push(newObject);
+      return;
+    });
+
+    await Promise.all(promises);
+    return result;
   }
 }
 
--- helpers/worker.js
+++ helpers/worker.js
@@ -1,4 +1,20 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
 const { workerData, parentPort } = require('worker_threads');
 
-console.log('Received data from main thread...');
-parentPort.postMessage({ link: workerData });
+(async function() {
+  let res;
+  try {
+    res = await axios.get(workerData.link);
+  } catch (error) {
+    console.log(error);
+  }
+  const $ = cheerio.load(res.data);
+  const link = $('#seller-view-more-link').attr('href');
+  console.log(`close to name of store: ${link}`);
+  
+  
+  console.log(`Worker #${workerData.workerIndex} starting...`);
+  parentPort.postMessage({ link: workerData });
+})();
+
